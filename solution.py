f327778f6f476f915c3c63179d4315cbc11dc112
gubi95 <happywitcher@gmail.com>
2018-10-23 17:38:22 +0200

    Task 1

--- solution.py
+++ solution.py
@@ -2,12 +2,21 @@
 Rozwiązania do laboratorium 1 z Obrazowania Biomedycznego.
 """
 import numpy as np
+from obpng import write_png
 
 """
 3 - Kwadrat
 """
+#start[1] = y, start[0] = x
 def square(size, side, start):
-    pass
+    mainSquare = (size, size)
+    image = np.zeros(mainSquare).astype(np.uint8)
+    image.fill(128)    
+    image[start[1]:start[1] + side, start[0]:start[0] + side] = 64
+    write_png(image, 'results/1_square.png')
+    print(image)
+
+square(512,128,(32,64))
 
 """
 3 - Koło
@@ -29,6 +38,7 @@ def nn_interpolation(source, new_size):
 
 """
 5 - Interpolacja dwuliniowa
+
 """
 def bilinear_interpolation(source, new_size):
     pass
