012453ed7d449de7ee4d49d311c2dd8b074674bd
gubi95 <happywitcher@gmail.com>
2018-10-23 17:51:40 +0200

    Task 2

--- solution.py
+++ solution.py
@@ -13,8 +13,7 @@ def square(size, side, start):
     image = np.zeros(mainSquare).astype(np.uint8)
     image.fill(128)    
     image[start[1]:start[1] + side, start[0]:start[0] + side] = 64
-    write_png(image, 'results/1_square.png')
-    print(image)
+    write_png(image, 'results/1_square.png')    
 
 square(512,128,(32,64))
 
@@ -22,7 +21,33 @@ square(512,128,(32,64))
 3 - Koło
 """
 def midcircle(size):
-    pass
+    mainSquare = (size[0], size[1])
+    image = np.zeros(mainSquare).astype(np.uint8)
+    image.fill(128)
+
+    radius = 0;
+    divider = 4
+    if size[0] < size[1]:
+        radius = size[0] / divider
+    else:
+        radius = size[1] / divider
+
+    xCenter = size[0] / 2
+    yCenter = size[1] / 2
+
+    radius = int(radius)
+    xCenter = int(xCenter)
+    yCenter = int(yCenter)
+
+    for x in range (xCenter - radius, xCenter + radius):
+        for y in range (yCenter - radius, yCenter + radius):
+            value = (x - xCenter)**2 + (y - yCenter)**2 <= radius**2
+            if value:
+                image[x,y] = 64
+    
+    write_png(image, 'results/2_circle_2.png')
+    
+midcircle((256,512))
 
 """
 3 - Szachownica.
